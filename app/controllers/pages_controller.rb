0c4a41f632fb9be55a4892d44dc41ab6382678b1
Anton Shevchuk <AntonShevchuk@gmail.com>
2017-03-20 14:55:03 +0200

    Added create form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,35 @@
 class PagesController < ApplicationController
   before_action :require_user, only: [:new, :edit, :update, :destroy]
-  def new
-    @page = Page.new
-  end
   def index
     @pages = Page.all
   end
   def show
     @page = Page.find(params[:id])
-    @older = Page.where('created_at < ?', @page.created_at).take
+    @older = Page.where('created_at < ?', @page.created_at).order('created_at DESC').take
     @newer = Page.where('created_at > ?', @page.created_at).take
+  end
+  def new
+    @page = Page.new
+  end
+  def create
+    @page = Page.new(page_params)
+    @page.user_id = session[:user_id]
+    if @page.save
+      flash[:notice] = "Page created"
+      redirect_to page_path @page
+    else
+      flash.now[:error] = "Please fix all errors"
+      render 'new'
+    end
   end
   def archive
     first = Date.new(params[:year].to_i, params[:month].to_i, 01)
     last = first + 1.month
     @pages = Page.where('created_at BETWEEN ? AND ?', first, last)
     @date = first.strftime('%B %Y')
+  end
+  private
+  def page_params
+    params.require(:page).permit(:title, :content, :category_id)
   end
 end
