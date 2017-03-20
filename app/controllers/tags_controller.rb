57338718586d3d6ca8ec10a07d54bb888cde514a
Anton Shevchuk <AntonShevchuk@gmail.com>
2017-03-20 19:16:45 +0200

    Added tag cloud

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
   before_action :require_user, only: [:new, :edit, :update, :destroy]
   def index
-    @tags = Tag.all
+    @tags = Tag.joins(:parts).group('tags.id').select('tags.id, tags.name, COUNT(*) as quantity')
   end
   def show
     @tag = Tag.find(params[:id])
