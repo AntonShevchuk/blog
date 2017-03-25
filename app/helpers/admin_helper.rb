f0ee034b6da840aee5dcc24687222476da8f9ba3
Anton Shevchuk <AntonShevchuk@gmail.com>
2017-03-25 21:28:12 +0200

    Updated helper

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,7 +2,7 @@ module AdminHelper
   def sortable(column, title = nil)
     title ||= column.titleize
     direction = (column == params[:sort] && params[:direction]) == "asc" ? "desc" : "asc"
-    css_class = (column == params[:sort]) ? "current #{direction}" : "asc"
+    css_class = (column == params[:sort]) ? "current #{direction}" : ""
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
 end
